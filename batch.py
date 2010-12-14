a0f49ab97ff332754118e7b1ed7e7e76b7df9941
pkeane <pkeane@mail.utexas.edu>
2010-12-14 13:28:55 -0600

    uploader!

--- batch.py
+++ batch.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 import getpass
 import mimetypes
+import os
 import sys
 import urllib2
 
-sys.path.append("/home/pkeane/Desktop/google_appengine")
-sys.path.append("/home/pkeane/Desktop/google_appengine/lib/fancy_urllib")
-sys.path.append("/home/pkeane/Desktop/google_appengine/lib/yaml/lib")
+GAE_PATH = "/home/pkeane/Desktop/google_appengine"
+sys.path.append(GAE_PATH)
+sys.path.append(GAE_PATH+"/lib/fancy_urllib")
+sys.path.append(GAE_PATH+"/lib/yaml/lib")
+
+MY_APP = 'simplerepo'
 
 from google.appengine.tools import appengine_rpc
 
@@ -37,22 +41,26 @@ def get_content_type(filename):
 def auth_func():
   return raw_input('Username: '), getpass.getpass('Password: ')
 
-server = appengine_rpc.HttpRpcServer('simplerepo.appspot.com',auth_func,None,'gae_login',save_cookies=True)
+def get_server(app_name,auth_function):
+  return appengine_rpc.HttpRpcServer(app_name+'.appspot.com',auth_function,None,'gae_login',save_cookies=True)
 
-url = server.Send('/collection/new_collection/upload_url',None) #None makes it GET, otherwise method is POST
+def get_upload_url(server,collection):
+  url = server.Send('/collection/'+collection+'/upload_url',None) #None makes it GET, otherwise method is POST
+  url = url.replace('https://'+MY_APP+'.appspot.com','')
+  return url
 
-url = url.replace('https://simplerepo.appspot.com','')
+def upload_file(server,filepath,collection):
+  filename = os.path.basename(filepath)
+  url = get_upload_url(server,collection)
+  data = open(filepath).read()
+  fields = []
+  files = [('file',filename,data)]
+  (content_type,payload) = encode_multipart_formdata(fields, files)
+  resp = server.Send(url,payload=payload,content_type=content_type)
+  return resp
 
-data = open('redbox.jpg').read()
-#data = open('pic.jpg').read()
-
-
-fields = []
-files = [('file','redbox.jpg',data)]
-#files = [('file','pic.jpg',data)]
-
-print "posting image"
-
-(content_type,payload) = encode_multipart_formdata(fields, files)
-
-print server.Send(url,payload=payload,content_type=content_type)
+if __name__=="__main__":
+  server = get_server(MY_APP,auth_func)
+  dir = '/home/pkeane/stv_histograms/img'
+  for file in os.listdir(dir):
+    print upload_file(server,dir+'/'+file,'new_collection')
